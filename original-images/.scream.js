9a67cae4b5c3a09aff2f54ab41593885272696e5
s-a <stephan.ahlf@googlemail.com>
2013-06-18 09:31:41 +0200

    First pages commit

---
+++ original-images/.scream.js
@@ -0,0 +1,26 @@
+var config = {
+	supportedPixelRatios : [1, 1.3, 1.5, 2, 2.1, 3],
+	spriteSheetPrefix : "sprite__",							// prefix of spritesheet filenames and classnames
+	settings : {
+		images : {
+			route : "/assets/images/",
+			targetCacheDirectory : __dirname + "/../assets/images"
+		},
+		css: {
+			route : "/assets/css/",
+			imageUrl : "../../images/",						// points to the target url of image directory. In this case relative to "/assets/css/{pixelResolution}"
+			targetCacheDirectory : __dirname + "/../assets/css"
+		}
+	},
+	images : {	
+		"dragonfly.png" : {										// alias
+			filename : __dirname + "/dragonfly-122794.png",		// original filename
+			sprite: "test",										// determines if the image should be included within specified sprite sheet
+			batch : [											// image processing shell scripts with paramters
+				"-resize", "x400"								// -size width[xheight][+offset]
+			]
+		} 
+	}
+};
+
+module.exports = config;
