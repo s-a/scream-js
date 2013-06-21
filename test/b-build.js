c4a2673a1ea45b62c04ed0c43be8eefcb4752317
s-a <stephan.ahlf@googlemail.com>
2013-06-21 08:14:06 +0200

    moved file exists

--- test/b-build.js
+++ test/b-build.js
@@ -1,18 +1,9 @@
 
 var ScreamProcess = require('./../lib/scream-process');
+var ScreamIO = require('./../lib/scream-io');
 var path = require('path');
 require('should');
-var fs = require('fs');
 
-
-var fileExists = function (filename) {
-  try {
-      stats = fs.lstatSync(filename);
-      return true;
-  } catch (e) {
-      return false;
-  }
-};
 
 describe('Build process',function(){
 
@@ -28,16 +19,16 @@ describe('Build process',function(){
 
   it('should have created CSS files', function(done){
       var css = path.join(__dirname, "/../www-root/assets/css/scream/");
-      fileExists(path.join(css, "scream.css")).should.be.true;
-      fileExists(path.join(css, "scream-js.css")).should.be.true;
+      ScreamIO.file.exists(path.join(css, "scream.css")).should.be.true;
+      ScreamIO.file.exists(path.join(css, "scream-js.css")).should.be.true;
       done();
   });
 
   it('should have created image files', function(done){
       var img = path.join(__dirname, "/../www-root/assets/images/scream/");
-      fileExists(path.join(img, "sprite__gui-background.png")).should.be.true;
-      fileExists(path.join(img, "sprite__page-background.png")).should.be.true;
-      fileExists(path.join(img, "brand-inverted-watermarked-nodejs-32.png")).should.be.true;
+      ScreamIO.file.exists(path.join(img, "sprite__gui-background.png")).should.be.true;
+      ScreamIO.file.exists(path.join(img, "sprite__page-background.png")).should.be.true;
+      ScreamIO.file.exists(path.join(img, "brand-inverted-watermarked-nodejs-32.png")).should.be.true;
       done();
   });
 
